b184244b896d80bbc66d55b7012ddbfa7888d8a8
Ray-Auditore <151702591+Ray-Auditore@users.noreply.github.com>
2024-08-06 13:38:17 +0700

    tugas05
    tugas 05 selasa

---
+++ meeting4/tugas05.js
@@ -0,0 +1,56 @@
+// nomer 1
+
+let hari = "senin";
+
+switch (hari) {
+  case "senin":
+    console.log("Weekday");
+    break;
+  case "selasa":
+    console.log("Weekday");
+    break;
+  case "rabu":
+    console.log("Weekday");
+    break;
+  case "kamis":
+    console.log("Weekday");
+    break;
+  case "jumat":
+    console.log("Weekday");
+    break;
+  case "sabtu":
+    console.log("Weekend");
+    break;
+  case "minggu":
+    console.log("Weekend");
+    break;
+}
+//nomer 2 
+for (let i = 1; i <= 15; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+  
+  
+  //nomer 3
+  for (let i = 0; i <= 10; i++) {
+    if (i % 2 === 0) {
+      console.log(i);
+    }
+  }
+
+//   nomer 4
+for (let i = 1; i <= 15; i++) {
+    if (i % 3 === 0) {
+      console.log("multipleofthree");
+    } else {
+      console.log(i);
+    }
+  }
