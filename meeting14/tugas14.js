87b877e5eabc73230a87c551d0b9ca2e1e8b6697
tiiilod <151702591+Ray-Auditore@users.noreply.github.com>
2024-10-07 10:32:32 +0700

    save

--- meeting14/tugas14.js
+++ meeting14/tugas14.js
@@ -67,4 +67,31 @@ async function getPostDetails() {
     getPostDetails();
     
 // soal 5
-save  
+async function getPostDetails() {
+  const result = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  const data = await result.json();
+  if (result.status === 200) {
+    console.log(data.title);
+  } else {
+    console.log("failed to fetch post");
+  }
+}
+
+getPostDetails();
+
+// soal 6
+async function checkPostAvailability() {
+  const result2 = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  const data2 = await result2.json();
+
+  let hasil = result2.userId > 5 ? "post is available for user IDs greater than 5" : "post is not available for user IDs greater than 5";
+  console.log(hasil);
+
+  if (result2.userId > 5) {
+    console.log("post is available for user IDs greater than 5");
+  } else {
+    console.log("post is not available for user IDs greater than 5");
+  }
+}
+
+checkPostAvailability(); 
